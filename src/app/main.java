20b1bbaa4259c4f9ac9684d0523748e51ea7297a
tatajjr <ivangcortes6@gmail.com>
2024-03-24 21:42:39 +0100

    Main despues de probar que todo correcto

--- src/app/main.java
+++ src/app/main.java
@@ -1,5 +1,27 @@
 package app;
 
-public class main {
+import modelos.Circulo;
+import modelos.Rectangulo;
 
+public class Main {
+    public static void main(String[] args) {
+        Rectangulo r1 = new Rectangulo();
+        Circulo c1 = new Circulo();
+        Rectangulo r2 = new Rectangulo(21, 34);
+        Circulo c2 = new Circulo(20);
+
+        System.out.println(c1);
+        
+        r2.calcularArea();
+        c2.calcularPerimetro();
+        
+        r1.getAltura();
+        
+        c2.setRadio(4);
+        
+        System.out.println(c2.getRadio());
+        
+        
+        
+    }
 }
