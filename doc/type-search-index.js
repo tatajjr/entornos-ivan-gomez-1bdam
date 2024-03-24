a875586c7e40a35c5eda0c0a84eaa3d8a937e98d
tatajjr <ivangcortes6@gmail.com>
2024-03-24 22:11:24 +0100

    Proyecto documentado con javadoc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"modelos","l":"Circulo"},{"p":"modelos","l":"Figura"},{"p":"app","l":"Main"},{"p":"modelos","l":"Rectangulo"}];updateSearchResults();
